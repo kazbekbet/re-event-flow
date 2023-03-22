5ef69350b0f19f35ee85c5f98c647eb7976453fd
kazbek.beteev <kazbek.beteev@accenture.com>
2023-03-22 16:49:25 +0300

    react counter example added.

---
+++ src/examples/react/src/counter/api.ts
@@ -0,0 +1,13 @@
+export function asyncCount(
+  countTo: number,
+  delay: number,
+  isReject: boolean
+): Promise<number> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      isReject
+        ? reject({ message: 'Error!' })
+        : resolve(countTo);
+    }, delay);
+  });
+}
