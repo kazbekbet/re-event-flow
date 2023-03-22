b528d180eaebf922297a5265b07c6026fd686c84
kazbek.beteev <kazbek.beteev@accenture.com>
2023-03-22 11:37:46 +0300

    use-sync-external-store import changed to shim.

--- src/react/hooks/use-store.ts
+++ src/react/hooks/use-store.ts
@@ -1,5 +1,5 @@
 import type { Store } from '../../core/store';
-import { useSyncExternalStore } from 'use-sync-external-store';
+import { useSyncExternalStore } from 'use-sync-external-store/shim';
 
 export function useStore<Val>(store: Store<Val>) {
   return useSyncExternalStore(store.subscribe, store.getState);
