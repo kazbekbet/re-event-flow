cdb9874cf0e9e1e36535caef736e753c5d83bba4
kazbek.beteev <kazbek.beteev@accenture.com>
2023-04-10 11:29:34 +0300

    handlers array added for redirect func.

--- src/core/store/impl/helpers.ts
+++ src/core/store/impl/helpers.ts
@@ -1,23 +1,35 @@
 import { SetEvent } from '../abstract/contracts';
 import { setEvent, setStore } from './index';
 
+// TODO: refactor.
+/**
+ * Event definition.
+ * */
+type Event<Payload> = SetEvent.Return<Payload>;
+
+type Handler<Payload> = (val: Payload) => unknown;
+
 /**
  * Creates a one-way link between event and handler.
  * If the event is fired then the handler will be fired.
  * If the event was not passed then the redirect create and return new event.
  *
- * @param {SetEvent.Return<Payload>} [event].
- * @param {Function} handler.
+ * @param {Event<T>} [event].
+ * @param {Handler<T> | Handler<T>[]} handler.
  * */
 export function redirect<Payload>({
   event = setEvent<Payload>(),
   handler,
 }: {
-  event?: SetEvent.Return<Payload>;
-  handler: (val: Payload) => unknown;
+  event?: Event<Payload>;
+  handler: Handler<Payload> | Handler<Payload>[];
 }) {
   setStore(handler).on(event, (memoizedHandler, payload) => {
-    memoizedHandler(payload);
+    if (Array.isArray(memoizedHandler)) {
+      memoizedHandler.forEach(handlers => handlers(payload));
+    } else {
+      memoizedHandler(payload);
+    }
 
     return memoizedHandler;
   });
