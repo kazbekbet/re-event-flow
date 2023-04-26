a765b254b56f44e9ded0fdf42bb2c0a4848309d4
kazbek.beteev <kazbek.beteev@accenture.com>
2023-04-26 10:37:38 +0300

    Re-export contracts.

--- src/core/store/index.ts
+++ src/core/store/index.ts
@@ -1,2 +1,3 @@
 export { setAsyncEvent, setEvent, setStore, setComputedStore, redirect } from './impl';
 export { Store } from './impl/store';
+export type { SetEvent, SetAsyncEvent, SetComputedStore } from './abstract/contracts';
